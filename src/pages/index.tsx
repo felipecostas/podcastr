39afe57567acda6cb4a6e35c9ab73c0102981590
Felipe Costa <contatofelipecsr@gmail.com>
2021-04-22 16:42:15 -0300

    context/audio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { useContext } from 'react';
 import { GetStaticProps } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -6,6 +7,7 @@ import ptBR from 'date-fns/locale/pt-BR';
 
 import { api } from '../services/api';
 import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
+import { PlayerContext } from '../contexts/PlayerContext';
 
 import styles from './home.module.scss';
 
@@ -13,11 +15,11 @@ type Episode = {
   id: string;
   title: string;
   thumbnail: string;
-  members: String;
+  members: string;
   duration: number;
   durationAsString: string;
   url: string;
-  publishedAt: String;
+  publishedAt: string;
 }
 
 type HomeProps = {
@@ -26,6 +28,8 @@ type HomeProps = {
 }
 
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
+  const {play } = useContext(PlayerContext)
+
   return (
     <div className= { styles.homepage }>
     <section className = { styles.latestEpisodes }>
@@ -52,8 +56,8 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                   <span>{episode.durationAsString}</span>
                 </div>
 
-                <button type="button">
-                  <img src="/play-green.svg" alt="Tocar episodio "/>
+                <button type="button" onClick={() => play(episode)}>
+                  <img src="/play-green.svg" alt="Tocar episódio "/>
                 </button>
               </li>
             )
